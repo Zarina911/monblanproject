501a66302fd2d41fc157681c0ad8eb263eea375a
Zarina Lavrenenko <zari.magura@gmail.com>
2019-08-17 11:48:32 +0300

    gallery add

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,6 @@
 
-$(document).ready(function () {
-    $(".owl-carousel.brands").owlCarousel({
-        loop: true,
-        items: 6,
-        responsive: {
-            0: {
-                items: 1
-            },
-            400: {
-                items: 2
-            },
-            600: {
-                items: 3
-            },
-            850: {
-                items: 5
-            },
-            1000: {
-                items: 6
-            }
-        }
-    });
-});
 
-$(document).ready(function () {
-    $(".owl-carousel.photo_carousel").owlCarousel({
-        loop: true,
-        margin: 30,
-        nav: false,
-        dots: false,
-        items: 1,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        stagePadding:30,
-        smartSpeed:450,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            400: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            850: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
